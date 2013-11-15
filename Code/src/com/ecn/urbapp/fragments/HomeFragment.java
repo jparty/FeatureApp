ebdb17f953328a67836dd39ef9d226523f8d3aca
Sebastien COHENDET <Sebastien.cohendet@centraliens-nantes.net>
2013-11-15 12:27:13 +0100

    Added a mean to load image from a server

--- Code/src/com/ecn/urbapp/fragments/HomeFragment.java
+++ Code/src/com/ecn/urbapp/fragments/HomeFragment.java
@@ -1,12 +1,22 @@
 package com.ecn.urbapp.fragments;
 
+import java.util.ArrayList;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.ecn.urbapp.R;
+import com.ecn.urbapp.utils.ImageDownloader;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 /**
  * This is the fragment used to make the user choose between the differents type of project.
@@ -22,6 +32,17 @@ import com.ecn.urbapp.R;
 
 public class HomeFragment extends Fragment{
 	
+	private Button downloadImage;
+	private final ImageDownloader imageDownloader = new ImageDownloader();
+	private ImageView image;
+	
+	private String[] URLs={
+			"http://static.tumblr.com/604c1f8526cf8f5511c6d7a5e32f9abd/u00yntv/2wEmlbf4d/tumblr_static_baby_otter.jpg",
+			"http://axemdo.files.wordpress.com/2010/07/loutre1.jpg",
+			"http://www.spaycificzoo.com/wp-content/uploads/2011/11/loutre_naine1-300x232.jpg"
+	};
+	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
@@ -29,6 +50,20 @@ public class HomeFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.layout_home, null);
+		downloadImage = (Button) v.findViewById(R.id.home_loadDistantlProject);
+		downloadImage.setOnClickListener(getImage);
+		image = (ImageView) v.findViewById(R.id.home_image_loadDistantProject);
 		return v;
 	}
+	
+    private OnClickListener getImage = new OnClickListener() {
+        
+        @Override
+        public void onClick(View v) {
+        	imageDownloader.download(URLs[(int) (Math.random()*3)], image);
+        }
+};
+
+
+	
 }
