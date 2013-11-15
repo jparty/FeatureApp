c505387a8f06b806137da1a5461530a34514c366
Sebastien COHENDET <Sebastien.cohendet@centraliens-nantes.net>
2013-11-15 12:42:24 +0100

    Correct bug of none correct branching

--- Code/src/com/ecn/urbapp/fragments/HomeFragment.java
+++ Code/src/com/ecn/urbapp/fragments/HomeFragment.java
@@ -1,12 +1,21 @@
 package com.ecn.urbapp.fragments;
 
+import java.util.ArrayList;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.ecn.urbapp.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 /**
  * This is the fragment used to make the user choose between the differents type of project.
@@ -22,6 +31,15 @@ import com.ecn.urbapp.R;
 
 public class HomeFragment extends Fragment{
 	
+	private Button downloadImage;
+	private ImageView image;
+	
+	private String[] URLs={
+			"http://static.tumblr.com/604c1f8526cf8f5511c6d7a5e32f9abd/u00yntv/2wEmlbf4d/tumblr_static_baby_otter.jpg",
+			"http://axemdo.files.wordpress.com/2010/07/loutre1.jpg",
+			"http://www.spaycificzoo.com/wp-content/uploads/2011/11/loutre_naine1-300x232.jpg"
+	};
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,6 +48,8 @@ public class HomeFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.layout_home, null);
+
 		return v;
 	}
+	
 }
